d4ef538d54a10e42f41eead2009d817a06a9f062
Harry Reeder <skull@tardis.ed.ac.uk>
2015-02-24 13:44:08 +0000

    Update some documentation strings

--- tardis/script.py
+++ tardis/script.py
@@ -1,13 +1,13 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='TARDIS Project Toolkit')
-    subparsers = parser.add_subparsers()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='Subcommands', description="TARDIS Project Toolkit")
 
-    adduser_p = subparsers.add_parser('adduser', description="Create a new user on TARDIS")
+    adduser_p = subparsers.add_parser('adduser', description="Create a new user on TARDIS", help="Create a new user on TARDIS")
     adduser_p.add_argument("username")
 
-    deluser_p = subparsers.add_parser('deluser', description="Delete a user from TARDIS")
+    deluser_p = subparsers.add_parser('deluser', description="Delete a user from TARDIS", help="Delete a user from TARDIS")
     deluser_p.add_argument("username")
 
     parser.parse_args()
